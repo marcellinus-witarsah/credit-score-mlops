f15510c9b34ba37f83399d5eb287dab2a7df4b81
Marcellinus Aditya Witarsah <aw.marcel@gmail.com>
2024-07-31 15:05:46 +0700

    add pipeline for evaluation by tracking metrics

--- credit_score_mlops/modeling/evaluate.py
+++ credit_score_mlops/modeling/evaluate.py
@@ -5,6 +5,7 @@ A module for model evaluation.
 import pandas as pd
 import typer
 import dvc.api
+from dvclive import Live
 from pathlib import Path
 from credit_score_mlops.config import MODELS_DIR, PROCESSED_DATA_DIR
 from credit_score_mlops.modeling import WOELogisticRegression
@@ -24,10 +25,6 @@ def main(
     params = dvc.api.params_show()
 
     target = params["target"]
-    train_scores_file = params["evaluate"]["train_scores_file"]
-    test_scores_file = params["evaluate"]["test_scores_file"]
-    # train_calibration_curve_file = params["evaluate"]["train_calibration_curve_file"]
-    # test_calibration_curve_file = params["evaluate"]["test_calibration_curve_file"]
 
     # 2. Load data
     train_df = pd.read_csv(train_file)
@@ -46,26 +43,32 @@ def main(
     model = WOELogisticRegression.from_file(model_file)
 
     # 4. Evaluate model performance
-    # y_score_train = model.predict_proba(X_train)[:, 1]
-    # y_score_test = model.predict_proba(X_test)[:, 1]
     train_scores = model.evaluate(X_train, y_train, "Training")
     test_scores = model.evaluate(X_test, y_test, "Testing")
 
     # 5. Save results
-    save_json(data=train_scores, path=train_scores_file)
-    # plot_calibration_curve(
-    #     y_true=y_train,
-    #     y_pred_proba=model.predict_proba(X_train)[:, 1],
-    #     model_name=model.__class__.__name__,
-    #     path=train_calibration_curve_file,
-    # )
-    save_json(data=test_scores, path=test_scores_file)
-    # plot_calibration_curve(
-    #     y_true=y_test,
-    #     y_pred_proba=model.predict_proba(X_test)[:, 1],
-    #     model_name=model.__class__.__name__,
-    #     path=test_calibration_curve_file,
-    # )
+    with Live(dir="reports", dvcyaml=None) as live:
+        for metric_name, value in train_scores.items():
+            live.log_metric(Path("train") / metric_name, value, plot=False)
+
+        for metric_name, value in test_scores.items():
+            live.log_metric(Path("test") / metric_name, value, plot=False)
+
+        live.log_sklearn_plot(
+            kind="calibration",
+            labels=y_train,
+            predictions=model.predict_proba(X_train)[:, 1],
+            name="train_calibration",
+            n_bins=10,
+        )
+        
+        live.log_sklearn_plot(
+            kind="calibration",
+            labels=y_test,
+            predictions=model.predict_proba(X_test)[:, 1],
+            name="test_calibration",
+            n_bins=10,
+        )
 
 
 if __name__ == "__main__":
